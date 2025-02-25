d79d7f88bd59dcde83567f68ff4b47fe7ef5e38f
Umudisly <112596403+Umudisly@users.noreply.github.com>
2025-02-24 21:24:10 -0500

    New

--- .github/workflows/bankend.tf
+++ .github/workflows/bankend.tf
@@ -1,7 +1,7 @@
 terraform{
     backend "s3"{
-        bucket = "github-action-bucket-3"
-        key = "myumudi"
+        bucket = "github-actions-bucket-44"
+        key = "file/"
         region = "us-east-1"
     }
 }
