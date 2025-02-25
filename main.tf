db449326bda4fe8ff53b07565ed3c2fb91dd83c4
Umudisly <112596403+Umudisly@users.noreply.github.com>
2025-02-25 10:42:08 -0500

    new update

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
   tags = {
-    Name = "updated my account"
+    Name = "Code not working"
       }
 }
